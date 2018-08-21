6efdff678bd27febc260a25cbf1963b7e18aa959
Duhamel Guerrero <duhamelgm@gmail.com>
2018-08-20 22:41:31 -0400

    actualizacion 20/08/2018

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,65 +1,98 @@
 /*
-
-    3D SHOWCASES
-
+    3D SHOWCASE
 */
 
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+var camera, renderer, light, mesh, clock;
+var mixer, animationClip;
 
-var renderer = new THREE.WebGLRenderer({ alpha:true });
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.getElementById("title-background").appendChild( renderer.domElement );
-renderer.setClearColor( 0x000000, 0);
+var loader = new THREE.GLTFLoader();
+loader.load(
+    'js/models/002.gltf',
 
-var geometry = new THREE.BoxGeometry( 2, 2, 2 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+    function(gltf){
+
+        mesh = gltf.scene;
+
+        mesh.traverse(function(child) {
+            if(child instanceof THREE.PerspectiveCamera)
+            {
+                camera = child;
+                console.log(child);
+
+                console.log(gltf.animations)
+
+                mixer = new THREE.AnimationMixer( mesh);
+                mixer.clipAction( gltf.animations[0]).play();
+
+            }
+        });
+
+        scene.add(mesh);
+        console.log(mesh);
+
+        modelLoaded();
+    }
+)
+
+function modelLoaded() {
+    init();
+    animate();    
+}
+
+function init() {
+    renderer = new THREE.WebGLRenderer({ alpha: true});
+    renderer.setClearColor( 0x000000, 0 );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.getElementById("title-background").appendChild( renderer.domElement );
 
 
-camera.position.z = 5;
+    light = new THREE.DirectionalLight( 0xFFFFFF );
+    scene.add( light );
+    light.target = mesh;
+    light.position.x = 1;
+    light.position.z = 1;
+
+    //var helper = new THREE.DirectionalLightHelper( light, 5 );
+    //scene.add( helper );
+
+    clock = new THREE.Clock();
+
+    window.addEventListener( 'resize', onWindowResize, false );
+
+    onWindowResize();
+}
+
+function onWindowResize() {
+    
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
 
 function animate() {
     requestAnimationFrame( animate );
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-	renderer.render( scene, camera );
-}
-animate();
-
-window.addEventListener("resize", onWindowResize, false);
-
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize( window.innerWidth, window.innerHeight);
+    
+    mixer.update( clock.getDelta() );
+    renderer.render( scene, camera );
 }
 
 /*
-
-    UI
-
+    UI SCRIPTS
 */
-//Title-Background
 
+//Title-Background
 var waypointTitleBackgroundTransition = new Waypoint({
     element: document.getElementById("about-content"),
     handler: function(){
         var titleBackground = document.getElementById("title-background");
         if(titleBackground.className === "transition") {
             titleBackground.className = "activated";
-            //titleBackground.style.position = "fixed";
-
-            camera.aspect = window.innerWidth / window.innerHeight;
-            camera.updateProjectionMatrix();
-            renderer.setSize( window.innerWidth , window.innerHeight);
+          
         } else {
             titleBackground.className = "transition";
-
-            camera.aspect = window.innerWidth * 0.5 / window.innerHeight;
-            camera.updateProjectionMatrix();
-            renderer.setSize( window.innerWidth * 0.5, window.innerHeight);
+            
         }
     },
     offset: "99%"
